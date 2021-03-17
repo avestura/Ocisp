29f3808a1073583dfccb552d4a4964bbf273f621
Aryan Ebrahimpour <me@avestura.dev>
2021-03-17 21:57:24 +0330

    Remove unused usings

--- Ocisp/MainWindow.xaml.cs
+++ Ocisp/MainWindow.xaml.cs
@@ -1,21 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 using OcispCore;
-using OcispCore.Matching;
-using OcispCore.DataStructure;
 using Microsoft.Win32;
 using System.Diagnostics;
 
